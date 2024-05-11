c8aeffba50af96927e2bdba5b7b5fbccf2cf09fd
Andrew Wong <wongwong.wong@gmail.com>
2024-05-10 18:55:27 -0600

    setup Router w/ child page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import Chat from './routes/chat/Chat'
+import ErrorPage from './ErrorPage'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "chat/:chatId",
+        element: <Chat />,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/chat/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Chat() {
+  /*
+    let { id } = useParams();
+      return <div>Now showing chat {id}</div>;
+  */
+  return (
+    <div> Moshimosh! This is the Chat Page </div>
+  )
+};
+

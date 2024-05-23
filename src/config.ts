0bf078f17202afb3baffae87bef674e507a979f2
Andrew Wong <wongwong.wong@gmail.com>
2024-05-23 08:51:27 -0600

    chore: format script

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL: string = 'http://localhost:5555';
-export const OAUTH_CLIENT_ID: string = '489350944755-bma5mgelbm6o29apo20ksv4chu69a3ku.apps.googleusercontent.com';
-export const OAUTH_REDIRECT_URI: string = 'http://localhost:5173/';
+export const API_BASE_URL: string = "http://localhost:5555";
+export const OAUTH_CLIENT_ID: string =
+  "489350944755-bma5mgelbm6o29apo20ksv4chu69a3ku.apps.googleusercontent.com";
+export const OAUTH_REDIRECT_URI: string = "http://localhost:5173/";

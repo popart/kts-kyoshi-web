2fc0dfbfa06862cdbc85b687cc105a8ab4690700
Andrew Wong <wongwong.wong@gmail.com>
2026-09-20 10:38:41 +0900

    update oauth client id

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // LOCAL
 export const API_BASE_URL: string = "http://localhost:5555";
 export const OAUTH_CLIENT_ID: string =
-  "489350944755-bma5mgelbm6o29apo20ksv4chu69a3ku.apps.googleusercontent.com";
+  "600426616792-qq3cfvrrvj7htde79unu211i5oap6h33.apps.googleusercontent.com";
 export const OAUTH_REDIRECT_URI: string = "http://localhost:5173/";
 
 // DEV
